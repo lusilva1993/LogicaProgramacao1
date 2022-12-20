a8f86412c54f4b6de68da9efc15bec216933453a
Luciana Ferreira da Silva <luciana.silva.1993@gmail.com>
2022-12-20 20:57:01 -0300

    Primeiro commit

---
+++ src/jogoDaVelha/Main.java
@@ -0,0 +1,25 @@
+package jogoDaVelha;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		String primeiroJogador = "O";
+		String segundoJogador = "X";
+
+		String[][] tabuleiro = new String[3][3];
+
+		int linhaTabuleiro = 0;
+		int colunaTabuleiro = 0;
+
+		boolean continuarJogando = true;
+
+		System.out.println("O primeiro jogador ficará com o X.");
+		
+	}
+
+		
+}
