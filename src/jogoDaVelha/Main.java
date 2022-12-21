08af9eaba2ec6d72a813b59b1417d16311de890d
Luciana Ferreira da Silva <luciana.silva.1993@gmail.com>
2022-12-20 21:02:36 -0300

    Criado método para imprimir tabuleiro

--- src/jogoDaVelha/Main.java
+++ src/jogoDaVelha/Main.java
@@ -18,8 +18,26 @@ public class Main {
 		boolean continuarJogando = true;
 
 		System.out.println("O primeiro jogador ficará com o X.");
+		imprimindoTabuleiro(tabuleiro);
 		
 	}
 
+	public static void imprimindoTabuleiro(String[][] tabuleiro) {
+		System.out.println("  0 1 2");
+		for (int i = 0; i < tabuleiro.length; i++) {
+			System.out.print(i + " ");
+			for (int j = 0; j < tabuleiro[i].length; j++) {
+				if (tabuleiro[i][j] == null) {
+					System.out.print(" ");
+				} else {
+					System.out.print(tabuleiro[i][j]);
+				}
+				if (j != tabuleiro[i].length - 1) {
+					System.out.print("|");
+				}
+			}
+			System.out.println();
+		}
+	}
 		
 }
